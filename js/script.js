a28978e438ca4f44dd7c73768918cac16a6dd143
haradaconnect <haradaconnect@gmail.com>
2020-02-04 18:13:00 +0900

    ラベル

--- js/script.js
+++ js/script.js
@@ -50,6 +50,7 @@ $(document).ready(function(){
   var mySwiper = new Swiper ('.swiper-container', {
     loop: true,
     pagination: '.swiper-pagination',
+    paginationClickable: true,
     nextButton: '.swiper-button-next',
     prevButton: '.swiper-button-prev',
     breakpoints: {
